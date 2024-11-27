04bdff0ab2635e170509d1e0efe742ad7d84de56
vEth <48117929+Haoxincode@users.noreply.github.com>
2024-11-27 13:51:31 +0000

    Add XAI Provider

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -8,18 +8,30 @@ export interface Model {
 }
 
 export const models: Array<Model> = [
-  {
+/*   {
     id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
+    label: 'GPT-4o mini',
     apiIdentifier: 'gpt-4o-mini',
     description: 'Small model for fast, lightweight tasks',
   },
   {
     id: 'gpt-4o',
-    label: 'GPT 4o',
+    label: 'GPT-4o',
     apiIdentifier: 'gpt-4o',
     description: 'For complex, multi-step tasks',
+  }, */
+  {
+    id: 'grok-beta',
+    label: 'Nebula Chat',
+    apiIdentifier: 'grok-beta',
+    description: 'Nebula chat for EEA development',
   },
+/*   {
+    id: 'grok-vision-beta',
+    label: 'Grok Vision Beta',
+    apiIdentifier: 'grok-vision-beta',
+    description: 'xAI\'s multimodal model supporting image input',
+  }, */
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: string = 'grok-beta';
